73aa96be6abc072836e79b083a5886b29e96d108
Codemurt <egor.lebe@inbox.ru>
2023-11-22 14:48:52 +0500

    SEO-optimization, new version of hunspell, ya-metric

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.responses import FileResponse
 
-ACTUAL_BASH_HUNSPELL_VERSION = "05.11.2023"
+ACTUAL_BASH_HUNSPELL_VERSION = "22.11.2023"
 
 app = FastAPI()
 
